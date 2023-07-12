e49e10b4487022a6e92e5f49fc858ca46c49643a
AlexandreGomesDaSilva <alexgds92@gmail.com>
2023-07-12 20:32:37 +0200

    Mise enMise en place de composer/altorouter, et de la BDD : OK

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'student/pokedex',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '25677289b45d98f5cb8fc73f999a20fbab76fc00',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'altorouter/altorouter' => array(
+            'pretty_version' => 'v1.2.0',
+            'version' => '1.2.0.0',
+            'reference' => '39c50092470128c12284d332bb57f306bb5b58e4',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../altorouter/altorouter',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'student/pokedex' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '25677289b45d98f5cb8fc73f999a20fbab76fc00',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
